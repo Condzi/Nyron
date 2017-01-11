25e5d269f213fe96f01363fb0bbf16d2e08af62a
Condzi <conradkubacki@gmail.com>
2017-01-11 19:27:26 +0100

    Moved load method in ResourceCache

--- source/ResourceCache.hpp
+++ source/ResourceCache.hpp
@@ -16,31 +16,7 @@ namespace cn
 		ResourceCache();
 		~ResourceCache() = default;
 
-		void load(const std::vector<std::string>& paths)
-		{
-			T* temporaryResource;
-			std::pair<std::uint16_t, std::uint16_t> loadedResourcesCount{ 0,paths.size() };
-
-			for (auto& path : paths)
-			{
-				temporaryResource = new T;
-
-				if (temporaryResource->loadFromFile(path))
-				{
-					Logger::log("Added new resource '" + path + "'", Logger::PREFIX_INFO);
-
-					resources[path].reset(temporaryResource);
-					++loadedResourcesCount.first;
-
-					continue;
-				}
-
-				delete temporaryResource;
-				Logger::log("Unable to load resource  '" + path + "'", Logger::PREFIX_ERROR);
-			}
-
-			Logger::log("Loaded " + std::to_string(loadedResourcesCount.first) + "/" + std::to_string(loadedResourcesCount.second) + " resources", Logger::PREFIX_INFO);
-		}
+		void load(const std::vector<std::string>& paths);
 
 		T* get(const std::string& name);
 	
@@ -51,6 +27,33 @@ namespace cn
 	template<class T>
 	inline ResourceCache<T>::ResourceCache()
 	{
+	}
+
+	template<class T>
+	inline void ResourceCache<T>::load(const std::vector<std::string>& paths)
+	{
+		T* temporaryResource;
+		std::pair<std::uint16_t, std::uint16_t> loadedResourcesCount{ 0,paths.size() };
+
+		for (auto& path : paths)
+		{
+			temporaryResource = new T;
+
+			if (temporaryResource->loadFromFile(path))
+			{
+				Logger::log("Added new resource '" + path + "'", Logger::PREFIX_INFO);
+
+				resources[path].reset(temporaryResource);
+				++loadedResourcesCount.first;
+
+				continue;
+			}
+
+			delete temporaryResource;
+			Logger::log("Unable to load resource  '" + path + "'", Logger::PREFIX_ERROR);
+		}
+
+		Logger::log("Loaded " + std::to_string(loadedResourcesCount.first) + "/" + std::to_string(loadedResourcesCount.second) + " resources", Logger::PREFIX_INFO);
 	}
 
 	template<class T>
